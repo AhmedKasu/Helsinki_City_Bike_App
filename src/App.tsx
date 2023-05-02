cb4fb7f7b516bb15e5d8b8f0342aad59b8965024
Ahmed Kasu <ankassu@gmail.com>
2023-05-02 15:10:38 +0300

    added app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { Grid, GridItem } from '@chakra-ui/react';
+
 function App() {
   return (
-    <>
-      <div>Helsinki city bike app</div>
-    </>
+    <Grid
+      templateAreas={`"header"
+                       "main"
+                      "footer"`}>
+      <GridItem pl='2' bg='orange.300' area={'header'}>
+        Header
+      </GridItem>
+      <GridItem pl='2' bg='green.300' area={'main'}>
+        Main
+      </GridItem>
+      <GridItem pl='2' bg='blue.300' area={'footer'}>
+        Footer
+      </GridItem>
+    </Grid>
   );
 }
 
