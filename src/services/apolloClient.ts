9d51d4cd1a5c7aa2183f8368baa2bb7443298463
Ahmed Kasu <ankassu@gmail.com>
2023-05-18 19:15:17 +0300

    configured server uri

--- src/services/apolloClient.ts
+++ src/services/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: import.meta.env.VITE_SERVER_URI,
 });
 
 const cache = new InMemoryCache({
